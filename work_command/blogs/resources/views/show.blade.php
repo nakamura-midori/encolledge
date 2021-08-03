8561eb013c0c9e385260d45fca7b165464713ee6
nakamura-midori <midonyanta714@gmail.com>
2021-08-04 00:34:25 +0900

    [追加]削除機能

--- work_command/blogs/resources/views/show.blade.php
+++ work_command/blogs/resources/views/show.blade.php
@@ -7,6 +7,12 @@
 
 <body>
     <p class-"edit">[<a href="/posts/{{$post->id}}/edit">edit</a>]</p>
+    <form action="/posts/{{ $post->id }}" id="form_delete" method="post" style="display:inline">
+        @csrf
+        @method('DELETE')
+        <input type="submit" style="display:none"></input> 
+        <p>[<span onclick="return deletePost(this)">delete</span>]</p>
+    </form>
     <!--<h1 class='title'>{{ $post->title }}</h1>-->
     <div class='post'>
         <h1 class='title'>{{ $post->title }}</h1>
@@ -16,5 +22,13 @@
     <div class='footer'>
         <a href="/">戻る</a>
     </div>
+    <script>
+        function deletePost(e){
+            'use strict'
+            if(confirm('削除すると復元できません．\n本当に削除しますか')){
+                document.getElementById('form_delete').submit();
+            }
+        }
+    </script>
 </body>
 </html>
